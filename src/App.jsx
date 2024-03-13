b727436e26e4ae08c5c8d21a5bfa8f245aae81bc
Xondamir <abdulazizovxondamirxon@gmail.com>
2024-03-13 11:42:07 +0500

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, Route, Routes } from "react-router-dom";
 
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("./pages/home.jsx"));
+const About = lazy(() => import("./pages/about.jsx"));
+const Contact = lazy(() => import("./pages/contact.jsx"));
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div style={{ display: "flex", gap: "5rem", justifyContent: "center" }}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<h2>Loading...</h2>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,67 @@
+
+
+import React from 'react'
+
+const Contact = () => {
+  return (
+     <div>
+      <h1>Contact</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo placeat
+      tempora repellat ea at, quibusdam blanditiis esse minima molestiae ipsam
+      nesciunt quam qui dolor dolorum possimus doloremque cupiditate. Sapiente
+      inventore a pariatur maxime dicta quia odio, nisi quidem velit dolorum,
+      dolorem cumque iure, temporibus quo aut placeat nostrum sit. Nisi, hic
+      consequatur? Dolorum quasi enim quia minima aliquid quas quaerat facere
+      possimus ab iure modi accusantium pariatur eos voluptas quos aspernatur
+      maiores alias temporibus animi, quo labore facilis. Maxime voluptas
+      debitis laboriosam nemo architecto numquam labore harum alias? Ut
+      asperiores, eius ullam, illo doloremque numquam maxime ab praesentium
+      porro quis suscipit sapiente nobis adipisci accusantium ad incidunt
+      consequuntur veritatis culpa. Itaque, magnam amet! Reprehenderit
+      consequatur quis numquam vitae in rerum cupiditate vero repellat tempora
+      saepe obcaecati ipsa rem deserunt voluptatum, inventore, omnis
+      perspiciatis odio optio quod exercitationem quia? Doloribus officiis,
+      possimus veniam maiores reiciendis delectus illo aspernatur soluta
+      obcaecati alias nesciunt esse dignissimos voluptas commodi sunt? Minima
+      eos fuga, architecto consequatur nostrum enim porro, perspiciatis
+      aspernatur quisquam accusantium fugit debitis doloremque distinctio error
+      animi totam. Repellat, inventore velit, iusto impedit rerum vel
+      exercitationem minima ratione eveniet voluptas aliquid perspiciatis unde
+      voluptatibus nulla delectus. Maiores, vero? Minus a, dolor magnam
+      assumenda totam doloribus mollitia ipsa iste non ipsam obcaecati illo
+      voluptates! Veritatis, culpa. Distinctio, ut atque, tempore officia nihil
+      quaerat blanditiis accusamus ratione quia ullam aliquid odit vero id ipsum
+      cumque, aperiam commodi saepe nisi aspernatur dolorem amet at non. Laborum
+      eaque aut voluptatem ipsa consequuntur sit iure quis, mollitia repudiandae
+      accusantium aperiam cumque voluptate possimus, tempora, nam rerum.
+      Asperiores voluptatum accusantium doloremque quam accusamus adipisci rem
+      doloribus iste a, laborum dolorem optio. Animi, tempora ab? Id blanditiis
+      nobis culpa quam illum architecto fugiat ea soluta magnam, nisi dicta,
+      ipsam, rerum earum nulla mollitia voluptates atque dolores sapiente
+      deserunt? Tenetur itaque tempora, consequuntur aliquam molestias nobis qui
+      perferendis blanditiis, dolore amet quasi ab natus doloremque sapiente.
+      Perferendis obcaecati fuga dolorem est autem possimus doloribus fugiat
+      nostrum, earum molestias ut dolorum tempore ipsam rem? Repellat nemo amet
+      aspernatur excepturi, alias error similique eius. Labore voluptatibus
+      quibusdam tempore necessitatibus natus, nihil consectetur sint, fugit,
+      sapiente obcaecati assumenda nemo? Repellendus, alias maxime. Quis
+      consectetur aliquam atque, culpa ad facilis praesentium quas cum eius sed
+      animi iusto deleniti excepturi natus in molestias ullam perferendis
+      voluptas sunt illo reiciendis beatae! Deleniti rem, cumque quam mollitia,
+      sit nostrum eos illo corporis cum accusantium harum sint assumenda
+      accusamus numquam perspiciatis magnam, culpa eaque odit vel labore est. Id
+      odio possimus quas? Et asperiores, fugiat praesentium obcaecati dolorum
+      quas distinctio, iusto sunt aspernatur earum in aliquid eligendi,
+      excepturi illo. Eligendi neque, aperiam iusto minus in natus quaerat
+      tempora corrupti aspernatur quo asperiores voluptate dignissimos animi
+      perferendis eveniet quisquam facilis eius corporis incidunt nesciunt ad
+      quibusdam voluptatum impedit? Consectetur ad error voluptatibus, dolor
+      quos officiis eligendi repellendus iste maxime nulla animi vel. Delectus
+      culpa fugit iste ea quidem, voluptatem repellendus architecto? Iste
+      sapiente dolor eum in vero reprehenderit accusamus quibusdam voluptatem
+      atque quas. Earum ex delectus natus dignissimos quia vitae.
+    </div>
+  )
+}
+
+export default Contact
